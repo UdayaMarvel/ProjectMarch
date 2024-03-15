5ce422a9f3920c8158bb03344c7de03b4cf8a941
UdayaMarvel <udayamarvel@gmail.com>
2024-03-15 16:58:45 +0530

    message

--- src/test/java/com/amazon/UdayaTask.java
+++ src/test/java/com/amazon/UdayaTask.java
@@ -3,13 +3,6 @@ package com.amazon;
 public class UdayaTask {
 
 	public static void main(String[] args) {
-		String name = "Udaya";
-		String rev = "";
-		for (int i = name.length()-1; i >=0 ; i--) {
-			char ch = name.charAt(i);
-			rev = rev+ch;
-		}
-		System.out.println(rev);
 		
 	}
 }
