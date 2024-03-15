0fadfff665a904ea67764980fb65c0e04302ec38
Srividya1696 <abinaya467@gmail.com>
2024-03-16 01:11:37 +0530

    palindrome program included

---
+++ src/test/java/com/amazon/StringPalindrome.java
@@ -0,0 +1,17 @@
+package com.amazon;
+
+public class StringPalindrome {
+	public static void main(String[] args) {
+		String name = "malayalam";
+		String reverseName = "";
+		for(int i=name.length()-1; i>=0;i--) {
+			reverseName = reverseName+ name.charAt(i);
+		}
+		if (name.toLowerCase().equals(reverseName.toLowerCase())) {
+		      System.out.println(name + " is a Palindrome String.");
+		    }
+		    else {
+		      System.out.println(name + " is not a Palindrome String.");
+		    }
+	}
+}
--- src/test/java/com/amazon/UdayaTask.java
+++ src/test/java/com/amazon/UdayaTask.java
@@ -3,7 +3,17 @@ package com.amazon;
 public class UdayaTask {
 
 	public static void main(String[] args) {
-		
+		String name = "malayalam";
+		String reverseName = "";
+		for(int i=name.length()-1; i>=0;i--) {
+			reverseName = reverseName+ name.charAt(i);
+		}
+		if (name.toLowerCase().equals(reverseName.toLowerCase())) {
+		      System.out.println(name + " is a Palindrome String.");
+		    }
+		    else {
+		      System.out.println(name + " is not a Palindrome String.");
+		    }
 		
 		
 		
