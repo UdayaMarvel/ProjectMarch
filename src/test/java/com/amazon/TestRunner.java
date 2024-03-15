622235ad2a2f1563eed9abf70d8093c46d4b4c1d
UdayaMarvel <udayamarvel@gmail.com>
2024-03-15 14:02:30 +0530

    first commit

---
+++ src/test/java/com/amazon/TestRunner.java
@@ -0,0 +1,11 @@
+package com.amazon;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TodaysDeals.class,TomorrowDeals.class})
+public class TestRunner {
+
+}
