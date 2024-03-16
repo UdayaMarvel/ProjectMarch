d03c934f01568e1caa6f99a945ec319c88925be4
SophiaRaj <rsophia1997@gmail.com>
2024-03-16 12:37:51 +0530

    Max

---
+++ src/test/java/com/amazon/Max.java
@@ -0,0 +1,36 @@
+package com.amazon;
+
+public class Max {
+
+	public static void main(String[] args) {
+		
+
+			   int a = 0 ;
+			   int b = 1 ; 
+			   int c ; 
+			   System.out.println(a);
+			   System.out.println(b);
+			   
+			   
+			   
+			   
+			   for (int i = 1 ; i<=5 ; i++) {
+				   
+			   c = a+b ; // 1
+			   
+			   System.out.println(c);
+			   
+			   a=b ;//1 
+			   b=c; //1
+
+			   
+			   }
+			   
+			   
+				}
+
+			
+
+	}
+
+
