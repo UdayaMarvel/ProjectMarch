1a9b31331be20ae679aab33eedc449c706025eff
Vickyvel1996 <vickyvel1996@gmail.com>
2024-03-17 10:52:42 +0530

    vigneshtask

---
+++ src/test/java/com/amazon/Vickyvel.java
@@ -0,0 +1,15 @@
+package com.amazon;
+
+public class Vickyvel {
+	
+ public static void main(String[] args) {
+	 String s = "udhaya bro task";
+	 int length = s.length();
+	 System.out.println(length);
+	 String replace = s.replace("udhaya", "vicky");
+	 System.out.println(replace);
+	 String uppercase = s.toUpperCase();
+	 System.out.println(uppercase);
+	
+}
+}
