8e329b044e9c210e760143b932c6963e66589410
praveenmurali22 <praveenmurali74@gmail.com>
2024-03-18 00:35:42 +0530

    praveenm

---
+++ src/test/java/com/amazon/praveenm.java
@@ -0,0 +1,25 @@
+package com.amazon;
+
+public class praveenm {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		{	
+		
+	}
+	System.out.println(a);
+	System.out.println(b);
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println("after swap");
+	System.out.println(a);
+	System.out.println(b);
+	}
+	  
+	}
+
+	
+
+
