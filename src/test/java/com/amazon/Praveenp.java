33be8b3510ef2293e10920290e374765addb1616
Praveenpalani03 <praveenpalaniedit@gmail.com>
2024-03-17 23:22:10 +0530

    Praveenp

---
+++ src/test/java/com/amazon/Praveenp.java
@@ -0,0 +1,20 @@
+package com.amazon;
+
+public class Praveenp {
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		int c =0;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		c=b;
+		b=a;
+		a=c;
+
+		System.out.println(a);
+		System.out.println(b);
+		}
+}
