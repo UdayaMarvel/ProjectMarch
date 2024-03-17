0740419cb4826f9b129f33cfd875edd2c517d19f
KasthuriBaiK <kasthuribaisubashini@gmail.com>
2024-03-17 21:15:25 +0530

    KasthuriBai commit

---
+++ src/test/java/com/amazon/SampleArray.java
@@ -0,0 +1,13 @@
+package com.amazon;
+
+public class SampleArray {
+	public static void main(String[] args) {
+
+		String S = "Sri";
+		char[] charArray = S.toCharArray();
+		for (int i = charArray.length-1; i>=0; i--) {
+			
+			System.out.print(charArray[i]);
+	}
+	}
+	}
