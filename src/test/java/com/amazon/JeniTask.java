f51d26a26788c5121b8d2cb347d6741ec56eb7e5
jeniprince <jeniblossom4@gmail.com>
2024-03-18 10:34:08 +0530

    First Commit

---
+++ src/test/java/com/amazon/JeniTask.java
@@ -0,0 +1,19 @@
+package com.amazon;
+
+public class JeniTask {
+	public static void main(String[] args) {
+		int a[]= {78,65,100,100,45};
+		int c;
+		for(int i = 0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				if(a[i]==a[j]) {
+					System.out.println(a[i]);
+				}
+
+				
+					
+				}					
+	}
+	}
+
+}
